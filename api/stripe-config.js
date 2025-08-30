2fa67b801e46a3963a137c6f44ab9053b24d3ddb
Donation-website <lipcseitundetimea@gmail.com>
2025-08-30 17:32:55 +0200

    Update stripe-config.js

--- api/stripe-config.js
+++ api/stripe-config.js
@@ -1,4 +1,3 @@
 module.exports = (req, res) => {
-// Visszaadjuk a publishable key-t a frontendnek
-res.status(200).json({ publishableKey: process.env.STRIPE_PUBLISHABLE_KEY || '' });
+  res.status(200).json({ publishableKey: process.env.STRIPE_PUBLISHABLE_KEY || '' });
 };
